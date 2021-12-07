79a683385ad9f067fd900154e3319bd115537452
SanjuSabu2 <sabusanju2@gmail.com>
2021-12-07 10:23:02 +0530

    Live Urls added

--- app/src/main/java/com/vkc/loyaltyme/api/ApiClient.kt
+++ app/src/main/java/com/vkc/loyaltyme/api/ApiClient.kt
@@ -6,10 +6,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private lateinit var apiService: ApiService
+    var BASE_URL = "http://dev.mobatia.com/vkc2.5/apiv2/" // Dev
+//    var BASE_URL = "https://mobile.walkaroo.in/vkc/" // Live
+//    var BASE_URL = "http://54.84.5.69/vkc/" // Live
+
     fun getApiService(): ApiService {
         val client = OkHttpClient.Builder().build()
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://dev.mobatia.com/vkc2.5/apiv2/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
